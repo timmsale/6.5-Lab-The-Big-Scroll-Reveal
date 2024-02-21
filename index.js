78090b7de4690160a8efffa293fc6570bdafebed
timmsale <155996877+timmsale@users.noreply.github.com>
2024-02-21 20:24:12 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Add JavaScript below
+window.onload = function(){
+
+ScrollReveal().reveal('.mesa', {delay: 700});
+ScrollReveal().reveal('.grand', {easing: 'ease-in', duration:700});
+ScrollReveal().reveal('.rushmore', {rotate: {x: 20,z: -40}, delay:1000});
+ScrollReveal().reveal('.hoover', {scale: 0.2, delay: 1500});
+ScrollReveal().reveal('.rocky', {opacity: 0.1, duration: 2000, delay:2500});
+}
